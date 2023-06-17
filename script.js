0bebe82d393d37e2645f4c9043ff3f74dcf508ee
kol3x <kolaegipet@gmail.com>
2023-06-17 15:14:30 +0300

    alfa version

---
+++ script.js
@@ -0,0 +1,19 @@
+// Gameboard module 
+
+const gameBoard = (() => {
+    let state = ['X', 'O', 'x',
+                 'X', 'X', 'O',
+                 'O', 'O', 'X'];
+    return {state};
+})();
+
+// Player factory func
+
+// const Player = (todo) => {
+    
+// };
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+});
+
